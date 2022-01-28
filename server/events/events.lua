796f96ed2983906b22dd57e2503ef346aafcc836
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-29 00:56:09 +0100

    Update events.lua

--- server/events/events.lua
+++ server/events/events.lua
@@ -1,4 +1,4 @@
 -- When the client request to refresh the vehicles
 RegisterServerEvent('qb-parking:server:refreshVehicles', function(parkingName)
-	RefreshVehicles(source)
+    RefreshVehicles(source)
 end)
