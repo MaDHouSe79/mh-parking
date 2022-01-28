6153c490b87362fac2e1aaa62a96f00436277ad9
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-29 00:59:31 +0100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -4,10 +4,7 @@ Config.Maxcarparking          = 50           -- max allowed cars in world space 
 Config.DisplayDistance        = 50           -- distence to see test above vehicles
 Config.SoundWhenCloseDoors    = false        -- sound when closing doors
 Config.parkingButton          = 166          -- F5 (vehicle exit and or park)
-
-
 Config.useRoleplayName        = true         -- if you want to use Roleplay name above the cars (firstname lastname) set this on true
-
 
 -- 👇 Base config when the server start
 Config.PhoneNotification      = true         -- 👉 Auto turn on when server is starting.
@@ -17,30 +14,30 @@ Config.HideParkedVehicleNames = false        -- 👉 default is true, if you wan
 
 
 Config.Command = {
-	park         = 'park',               -- User/Admin permission
-	parknames    = 'park-names',         -- User/Admin permission
-	notification = 'park-notification',  -- User/Admin permission
-	vip          = 'park-vip',           -- Admin permission
-	system       = 'park-system',        -- Admin permission
+    park         = 'park',               -- User/Admin permission
+    parknames    = 'park-names',         -- User/Admin permission
+    notification = 'park-notification',  -- User/Admin permission
+    vip          = 'park-vip',           -- Admin permission
+    system       = 'park-system',        -- Admin permission
 }
 
 Config.VipPlayers = {                        -- Add more vip plaers if you want.
-	[1] = {
-		username  = "MaDHouSe",      -- Just to know who it is
-		citizenid = "AWC63661",      -- you gen cet this from the databse players table copy citizenid and add this here
-		isAdmin   = true,
-	},
+    [1] = {
+	username  = "MaDHouSe",      -- Just to know who it is
+	citizenid = "AWC63661",      -- you gen cet this from the databse players table copy citizenid and add this here
+	isAdmin   = true,
+    },
 
-	[2] = {                              -- example player 2 to add more players as VIP
-		username  = "changeme",
-		citizenid = "changeme",
-		isAdmin   = false,
-	},
-	[3] = {                              -- example player 3 to add more players as VIP
-		username  = "changeme",
-		citizenid = "changeme",
-		isAdmin   = false,
-	}, -- just add more here
+    [2] = {                              -- example player 2 to add more players as VIP
+	username  = "changeme",
+	citizenid = "changeme",
+	isAdmin   = false,
+    },
+    [3] = {                              -- example player 3 to add more players as VIP
+	username  = "changeme",
+	citizenid = "changeme",
+	isAdmin   = false,
+    }, -- just add more here
 }
 
 Config.ParkingLocation = {x = 232.11, y = -770.14, z = 0.0, w = 900.10, s = 99999099.0}
