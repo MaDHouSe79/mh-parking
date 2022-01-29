5ae799210d0b497aecdd8675ab5edbebacca3e78
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-29 11:22:04 +0100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,18 +1,18 @@
 Config                        = Config or {}
 Config.debug                  = false
 Config.Maxcarparking          = 50           -- max allowed cars in world space (Default, dont go to hight)
-Config.DisplayDistance        = 50           -- distence to see test above vehicles
-Config.SoundWhenCloseDoors    = false        -- sound when closing doors
+Config.DisplayDistance        = 50           -- distence to see text above parked vehicles (player dependent)
+Config.SoundWhenCloseDoors    = false        -- if you want sound when closing vehicles doors
 Config.parkingButton          = 166          -- F5 (vehicle exit and or park)
 Config.useRoleplayName        = true         -- if you want to use Roleplay name above the cars (firstname lastname) set this on true
 
--- 👇 Base config when the server start
-Config.PhoneNotification      = true         -- 👉 Auto turn on when server is starting.
-Config.UseParkingSystem       = true         -- 👉 Auto turn on when server is starting.
-Config.OnlyAllowVipPlayers    = true         -- 👉 I recommend, to use this mod only for vip players....
-Config.HideParkedVehicleNames = false        -- 👉 default is true, if you want to see names just type /park-names on/off if you set this to true it is auto on 
+-- 👇 Base config when the server start, this is the default settings
+Config.PhoneNotification      = true         -- 👉 Auto turn on when server is starting. (default true)
+Config.UseParkingSystem       = true         -- 👉 Auto turn on when server is starting. (default true)
+Config.OnlyAllowVipPlayers    = true         -- 👉 I recommend, to use this mod only for vip players.... (default true)
+Config.HideParkedVehicleNames = false        -- 👉 default is false, if you want to see names just type /park-names on/off if you set this to true it is auto on 
 
-
+-- change rhis to your own commands
 Config.Command = {
     park         = 'park',               -- User/Admin permission
     parknames    = 'park-names',         -- User/Admin permission
@@ -21,6 +21,7 @@ Config.Command = {
     system       = 'park-system',        -- Admin permission
 }
 
+-- vip table
 Config.VipPlayers = {                        -- Add more vip plaers if you want.
     [1] = {
 	username  = "MaDHouSe",      -- Just to know who it is
@@ -40,4 +41,5 @@ Config.VipPlayers = {                        -- Add more vip plaers if you want.
     }, -- just add more here
 }
 
+-- Dont change this, you will not be able to park if you change this...
 Config.ParkingLocation = {x = 232.11, y = -770.14, z = 0.0, w = 900.10, s = 99999099.0}
