fc6456815671b6f5dc3cf586b9889de390a30d04
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-29 14:15:05 +0100

    Update impound.lua

--- client/actions/impound.lua
+++ client/actions/impound.lua
@@ -4,6 +4,7 @@ function ImpoundVehicle(vehicle)
 	if vehicle == LocalVehicles[i].entity then
 	    QBCore.Functions.TriggerCallback("qb-parking:server:impound", function(callback)
 		if callback.status then
+		    FreezeEntityPosition(LocalVehicles[i].entity, false)
 		    DeleteEntity(LocalVehicles[i].entity)
 		    table.remove(LocalVehicles, i)
 		end
