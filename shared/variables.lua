b40a119f5da639a0bf0740803168e04d5cfa50c0
MaDHouSe79 <madhouse1979@gmail.com>
2022-02-09 14:33:23 +0100

    Delete shared directory

--- shared/variables.lua
+++
@@ -1,6 +0,0 @@
--- Shared By Server And Client.
-UseParkingSystem       = Config.UseParkingSystem
-PhoneNotification      = Config.PhoneNotification
-HideParkedVehicleNames = Config.HideParkedVehicleNames
-curVersion             = 0
-responseText           = 0
