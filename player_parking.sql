e85aeca89f26bc3cc8bc015773181d96a3f387ab
MaDHouSe79 <madhouse1979@gmail.com>
2022-02-24 12:30:27 +0100

    .

--- player_parking.sql
+++ player_parking.sql
@@ -12,18 +12,6 @@ CREATE TABLE `player_parking`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 
-CREATE TABLE `player_parking_trailers`  (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `citizenname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `model` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `modelname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `plate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `time` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
-
-
 CREATE TABLE IF NOT EXISTS `player_parking_vips` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
