a4b0c07d19606602211209df9b46fc8ce43ceadf
MaDHouSe79 <madhouse1979@gmail.com>
2022-02-28 23:09:58 +0100

    .

--- player_parking.sql
+++ player_parking.sql
@@ -8,6 +8,7 @@ CREATE TABLE `player_parking`  (
   `fuel` int(15) NOT NULL DEFAULT 0,
   `oil` int(15) NOT NULL DEFAULT 0,
   `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `coords` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
   `time` bigint(20) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
