3db9ff22010f889c89e090e3ce25dd5b20bac413
MaDHouSe79 <madhouse1979@gmail.com>
2022-02-28 12:36:33 +0100

    table update

--- player_parking.sql
+++ player_parking.sql
@@ -1,16 +1,17 @@
-CREATE TABLE `player_parking`  (
+CREATE TABLE IF NOT EXISTS `player_parking` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `citizenname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `model` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `modelname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `plate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `fuel` int(15) NOT NULL DEFAULT 0,
-  `oil` int(15) NOT NULL DEFAULT 0,
-  `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `citizenid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `citizenname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'unknow',
+  `modelname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'unknow',
+  `plate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `data` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `time` bigint(20) NOT NULL,
+  `fuel` int(15) NOT NULL DEFAULT 100,
+  `oil` int(15) NOT NULL DEFAULT 100,
+  `coords` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=235 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 
 CREATE TABLE IF NOT EXISTS `player_parking_vips` (
