ba2b8c7807b089f45e91642a6b1f248074af44cc
MaDHouSe79 <madhouse1979@gmail.com>
2022-03-01 14:59:01 +0100

    Add files via upload

---
+++ configs/free.lua
@@ -0,0 +1,25 @@
+
+-- test created by MaDHouSe
+Config.ReservedParkList["test"] = {
+    ["display"]    = "test",
+    ["citizenid"]  = "0",
+    ["cost"]       = 0,
+    ["job"]        = "none",
+    ["radius"]     = 2.0,
+    ["parktype"]   = "free",
+    ["marker"]     = true,
+    ["coords"]     = vec3(222.778015, -801.626343, 30.307739),
+    ["markcoords"] = vec3(220.462494, -800.707092, 30.383707),
+}
+-- sadsadsad created by MaDHouSe
+Config.ReservedParkList["sadsadsad"] = {
+    ["display"]    = "dsadasda",
+    ["citizenid"]  = "0",
+    ["cost"]       = 0,
+    ["job"]        = "none",
+    ["radius"]     = 2.0,
+    ["parktype"]   = "free",
+    ["marker"]     = true,
+    ["coords"]     = vec3(221.894501, -804.145081, 30.324585),
+    ["markcoords"] = vec3(219.589157, -803.171997, 30.396965),
+}
