d0fdccb68f58fa8ae78b254e25f7549b134f002e
MaDHouSe79 <madhouse1979@gmail.com>
2022-03-08 13:26:32 +0100

    Update prived.lua

--- configs/prived.lua
+++ configs/prived.lua
@@ -1,1 +1,14 @@
-
+-- prived1 created by MaDHouSe (Date 2022/03/06 Time 15:00:39)
+Config.ReservedParkList["prived1"] = {
+    ["name"] = "prived1",
+    ["display"] = "MaDHouSe",
+    ["citizenid"] = "RJS90852",
+    ["coords"] = vec3(219.929672, -809.129700, 30.324585),
+    ["cost"] = 0,
+    ["parktime"] = 0,
+    ["job"] = "none",
+    ["radius"] = 2.0,
+    ["parktype"] = "prived",
+    ["marker"] = true,
+    ["markcoords"] = vec3(217.612640, -808.208313, 30.400316),
+}
