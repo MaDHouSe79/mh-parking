dc2a84fe72bf05c2cbc0f903244e9b58dc1a65b0
MaDHouSe79 <madhouse1979@gmail.com>
2022-07-09 09:02:37 +0200

    Update player_parking.sql

--- player_parking.sql
+++ player_parking.sql
@@ -25,19 +25,3 @@ CREATE TABLE IF NOT EXISTS `player_parking_vips` (
   `citizenname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
-
-
-/*NOT IN USE YET, YOU DONT HAVE TO ADD THIS*/
-CREATE TABLE IF NOT EXISTS `player_parking_reserved` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `label` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cost` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `radius` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `parktype` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `marker` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `coords` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `markcoords` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `time` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=235 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
