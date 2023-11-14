53302235dd374cee0869e89bd6e3bc24042d76f3
MaDHouSe <madhouse1979@gmail.com>
2023-11-14 21:03:28 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,12 +12,24 @@ ui_page 'html/index.html'
 
 files {'html/*.html', 'html/*.js', 'html/*.css'}
 
-shared_scripts {'@qb-core/shared/locale.lua', '@qb-core/shared/vehicles.lua', 'locales/en.lua', -- change en to your language
-                'config.lua', 'configs/*.lua'}
+shared_scripts {
+  '@qb-core/shared/locale.lua',
+  '@qb-core/shared/vehicles.lua',
+  'locales/en.lua', -- change en to your language
+  'config.lua', 
+  'configs/*.lua'
+}
 
-client_scripts {'client/main.lua'}
+client_scripts {
+  'client/main.lua'
+}
 
-server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
+server_scripts {
+  '@oxmysql/lib/MySQL.lua', 
+  'server/install.lua', -- uncomment this after you restart your server, so it will not run the second time.
+  'server/main.lua', 
+  'server/update.lua'
+}
 
 dependencies {'oxmysql', 'qb-core'}
 
