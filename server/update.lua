1cb717bae0413c29c99fd4eba1da4df954c555df
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 22:02:49 +0100

    Update update.lua

--- server/update.lua
+++ server/update.lua
@@ -8,9 +8,11 @@ CreateThread(function()
             version = string.gsub(version, "%s+", "")
             text = string.gsub(text, "%s+", "")
             if version == text then
-                print("^0[^4Update Check^0: ^1"..script.."^0] - [Installed Version:^2 "..version.."^0] [Github Version:^2 "..text.."^0] [Status:^2Ok^0] ")
-            else
-                print("^0[^4Update Check^0: ^1"..script.."^0] - Newer version of "..script.." found [Old:^1 "..version.."^0] [New:^2 "..text.."^0]")      
+                print("^0[^4Update Check^0: ^1"..script.."^0] - Current Version: ^2 "..version.."^0] [Github Version:^2 "..text.."^0] [Status:^2Ok^0] ")
+            elseif version < text then
+                print("^0[^4Update Check^0: ^1"..script.."^0] - Newer version of "..script.." found [Installed:^1 "..version.."^0] [New:^2 "..text.."^0]")
+            elseif version > text then
+                print("^0[^4Update Check^0: ^1"..script.."^0] - You somehow skipped a few versions of "..script.." or the github went offline, if it's still online i advise you to update ( or downgrade? )")
             end
         else
             print("[^6" .. script .. "^0] Check for script update ^1FAILED^0, unable to find the host.")
