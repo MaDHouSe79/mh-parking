3bcb46929b03a14b4539e274279f5cbbf186b978
MaDHouSe <madhouse1979@gmail.com>
2024-11-01 13:24:43 +0100

    Update update.lua

--- server/update.lua
+++ server/update.lua
@@ -10,7 +10,7 @@ CreateThread(function()
             if version == text then
                 print("^0[^2" .. script .. "^0] - [^3Update Check^0] - [Installed Version: ^2"..version.."^0] [Github Version:^2 "..text.."^0] [Status:^2Ok^0]")
             elseif version < text then
-                print("^0[^2" .. script .. "^0] - [^3Update Check^0] - Newer version found [Installed Version: ^2"..version.."^0] [Github Version:^2 "..text.."^0] [Status:^1Need Update^0]")
+                print("^0[^2" .. script .. "^0] - [^3Update Check^0] - Newer version found [Installed Version: ^2"..version.."^0] [Github Version:^2 "..text.."^0] [Status:^1Outdated^0]")
             elseif version > text then
                 print("^0[^2" .. script .. "^0] - [^3Update Check^0] - You somehow skipped a few versions or the github went offline, if it's still online i advise you to update ( or downgrade? )^0")
             end
