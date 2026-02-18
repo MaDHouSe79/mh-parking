d21983285be266cc9a7789fed2b3cca3e63b07dd
MaDHouSe <madhouse1979@gmail.com>
2026-02-18 10:43:19 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -341,14 +341,14 @@ end
 
 if Framework.name == 'esx' then
 
-    Framework.obj.RegisterCommand(Config.AddVipCommand, "admin", function(xPlayer, args, showError)
+    Framework.obj.RegisterCommand(Config.AddVipCommand, "admin", function(source, args, showError)
         local src, amount, targetID = source, SV_Config.DefaultMaxParking, -1
         if args[1] and tonumber(args[1]) > 0 then targetID = tonumber(args[1]) end
         if args[2] and tonumber(args[2]) > 0 then amount = tonumber(args[2]) end
         if targetID ~= -1 then AddVIP(src, targetID, amount) end
     end, false)
 
-    Framework.obj.RegisterCommand(Config.RemoveVipCommand, "admin", function(xPlayer, args, showError)
+    Framework.obj.RegisterCommand(Config.RemoveVipCommand, "admin", function(source, args, showError)
         local src, targetID = source, -1
         if args[1] and tonumber(args[1]) > 0 then targetID = tonumber(args[1]) end
         if targetID ~= -1 then RemoveVIP(src, targetID) end
@@ -369,4 +369,5 @@ elseif Framework.name == 'qb' or Framework.name == 'qbx' then
         if targetID ~= -1 then RemoveVIP(src, targetID) end
     end, 'admin')
 end
+
 
