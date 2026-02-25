696ab2b06ff5f6f4b0e78e638f9b9d7923dec2e1
MaDHouSe <madhouse1979@gmail.com>
2026-02-25 14:55:42 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -230,9 +230,13 @@ RegisterNetEvent('mh-parking:server:PayWheelclampBill', function(netid, plate)
                 if RemoveMoney(src, SV_Config.ClampFine) then
                     PayPoliceBill(SV_Config.ClampFine, 'wheelclamp-fine')
                     Database.UpdateWheelClamp(plate, false)
-                    Entity(veh).state.isClamped = false
-                    Notify(src, Lang:t('info.wheel_clamp_deleted'), 'success')
-                    TriggerClientEvent('mh-parking:syncWheelClamp', -1, netid)
+                    local wheel_clamp = NetworkGetEntityFromNetworkId(netid)
+                    if DoesEntityExist(wheel_clamp) then
+                        --TriggerClientEvent('mh-parking:syncWheelClamp', -1, netid)
+                        Entity(veh).state.isClamped = false   
+                        DeleteEntity(wheel_clamp)
+                        Notify(src, Lang:t('info.wheel_clamp_deleted'), 'success')   
+                    end
                 else
                     Notify(src, Lang:t('info.no_money', {money = SV_Config.MoneySign..SV_Config.ClampFine}), 'error')
                 end
@@ -369,5 +373,6 @@ elseif Framework.name == 'qb' or Framework.name == 'qbx' then
         if targetID ~= -1 then RemoveVIP(src, targetID) end
     end, 'admin')
 end
+
 
 
